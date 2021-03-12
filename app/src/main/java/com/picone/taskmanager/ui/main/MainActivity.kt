ab3fcf0e7db2527f998acffa6198ebefa72e5a08
JustAnOtherCoder13 <justanothercoder13@gmail.com>
2021-03-12 12:25:56 +0100

    create module and packages

--- app/src/main/java/com/picone/taskmanager/ui/main/MainActivity.kt
+++ app/src/main/java/com/picone/taskmanager/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.picone.taskmanager
+package com.picone.taskmanager.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.picone.taskmanager.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/picone/taskmanager/ui/main/TaskManagerApplication.kt
@@ -0,0 +1,6 @@
+package com.picone.taskmanager.ui.main
+
+import android.app.Application
+
+class TaskManagerApplication: Application() {
+}
