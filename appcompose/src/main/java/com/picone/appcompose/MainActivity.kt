fae37d16c90344af708c09926cf847d7d443e087
JustAnOtherCoder13 <justanothercoder13@gmail.com>
2021-05-03 20:44:02 +0200

    create compose app module and link to view models

--- appcompose/src/main/java/com/picone/appcompose/MainActivity.kt
+++ appcompose/src/main/java/com/picone/appcompose/MainActivity.kt
@@ -2,10 +2,24 @@ package com.picone.appcompose
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
+import com.picone.viewmodels.TaskViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.scopes.ActivityScoped
 
+@ActivityScoped
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val taskViewModel: TaskViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        taskViewModel.getAllTasks()
+        taskViewModel.mAllTasksMutableLD.observe(this,){
+            Log.i("TAG", "onCreate: "+ (it[0]))
+        }
     }
 }
---
+++ appcompose/src/main/java/com/picone/appcompose/TaskManagerComposeApplication.kt
@@ -0,0 +1,8 @@
+package com.picone.appcompose
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TaskManagerComposeApplication: Application() {
+}
